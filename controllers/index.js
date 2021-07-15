4cb1cfc6288ae6cf943d66ce93b283996981dc8c
raddymaster <antellitocci@gmail.com>
2021-07-14 20:41:00 -0400

    merge resolution 2

--- controllers/index.js
+++ controllers/index.js
@@ -1,17 +1,7 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-    res.status(404).end();
-});
-=======
 
 const apiRoutes = require('./api/');
 
 router.use('/api', apiRoutes);
->>>>>>> 97249d04497d7172f3828422dfee10e44425cc00
 
 module.exports = router;
