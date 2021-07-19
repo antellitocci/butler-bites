e4414b45c9606afef216d85b7308bcefc1acdf89
J2021T <80201567+J2021T@users.noreply.github.com>
2021-07-19 18:58:14 -0400

    added homepage route for category

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Recipe, User, Comment, Rating, Category } = require('../models');
+const { Recipe, User, Comment, Rating, Category, Ingredient } = require('../models');
 
 router.get('/', (req, res) => {
     Recipe.findAll({
@@ -40,7 +40,54 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/post/:id', (req, res) => {
+// gets category recipes
+router.get('/category/:id', (req, res) => {
+    Recipe.findAll({
+        where: {
+            category_id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'user_id',
+            'created_at',
+            'prep_time',
+            'cook_time'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Rating,
+                attributes: ['score']
+            },
+            {
+                model: Ingredient,
+                attributes: ['name', 'measurement']
+            }
+        ]
+    })
+    .then(dbRecipeData => {
+        if(!dbRecipeData) {
+            res.status(404).json({ message: 'No recipes found in that category' });
+            return;
+        }
+        const recipes = dbRecipeData.map(recipe => recipe.get({ plain:true }));
+        res.render('homepage', {
+            recipes,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// gets specific recipe
+router.get('/recipe/:id', (req, res) => {
     Recipe.findOne({
         where: {
             id: req.params.id
@@ -53,10 +100,14 @@ router.get('/post/:id', (req, res) => {
             'prep_time',
             'cook_time',
             'serving_size',
-            'ingredients',
+            'ingredient_id',
             'directions'
         ],
         include: [
+            {
+                model: Ingredient,
+                attributes: ['name', 'measurement']
+            },
             {
                 model: Comment,
                 attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
