642e8020c97b1c3bc1849398c8cea68467ad8e98
J2021T <80201567+J2021T@users.noreply.github.com>
2021-07-20 19:38:47 -0400

    added login.handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-// const userRoutes = require('./user-routes');
+const userRoutes = require('./user-routes');
 const recipeRoutes = require('./recipe-routes');
-// const commentRoutes = require('./comment-routes');
+const commentRoutes = require('./comment-routes');
 
-// router.use('/users', userRoutes);
+router.use('/users', userRoutes);
 router.use('/recipes', recipeRoutes);
-// router.use('/comments', commentRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -153,4 +153,13 @@ router.get('/recipe/:id', (req, res) => {
     });
 });
 
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/dashboard');
+      return;
+    }
+  
+    res.render('login');
+});
+
 module.exports = router;
